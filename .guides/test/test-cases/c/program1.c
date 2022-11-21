8a8f895e6bd774c1483635b59860ad2611ed405d
powcoder <powcoder@163.com>
2022-11-21 12:13:27 +0800

    CIT 593 module 11  c   strings

---
+++ .guides/test/test-cases/c/program1.c
@@ -0,0 +1,32 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+/*************************************************************************/
+/* File Name : program1.c 											     */
+/* Purpose   : this C-file tests the functions of the "my_string" library*/
+/*                                                                       */
+/*                                                                       */
+/* Author(s) : tjf & you 											     */
+/*************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include "../src/my_string.h"
+
+int main() {
+
+	// a string we will test with
+	char my_string [100] = "Tom" ;
+
+	// test of strlen() functions 
+	printf ("Sample string: \"%s\" \n", my_string) ;
+	printf (" string's length: strlen()     = %lu \n",   strlen  (my_string)) ;
+	printf (" string's length: my_strlen()  = %lu \n", my_strlen (my_string)) ;
+	printf (" string's length: my_strlen2() = %lu \n", my_strlen2(my_string)) ;
+
+    /* CIT 593 students: TODO: add code to test your my_string.h functions */
+
+	return (0) ;
+
+}
